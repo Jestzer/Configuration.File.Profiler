4e75ba7953f7a43cdadba7a19116f47b7047fe03
Jestzer <james@buonopane.email>
2024-01-30 22:29:06 -0500

    Chooses shared file system based on file name and/or a job struct being present. Fixed job structs. Simplied OS definitions. Deleted some comments. Some other things.

--- integration_scripts_profiler.m
+++ integration_scripts_profiler.m
@@ -59,9 +59,8 @@ else
     error('No .conf file found in the same directory as this function. Exiting.');
 end
 
-% Determine the operating system.
-isWindows = ispc;
-isUnix = isunix || ismac;
+% Combine macOS and Linux.
+isUnixBased = isunix || ismac;
 
 % Open the .conf file for reading
 fileID = fopen(confFileName, 'r');
@@ -73,6 +72,7 @@ try
     % Create the profile.
     clusterName = '';
     c = parallel.cluster.Generic;
+    HasSharedFilesystem = false; % We'll change this if needed.
 
     % Set all general cluster profile properties by reading line by line BEFORE [AdditionalProperties] is found.
     while ~feof(fileID)
@@ -108,28 +108,29 @@ try
                 end
 
                 % Check for OS-specific properties.
-                if contains(propertyName, '(Windows)') && isWindows
+                if contains(propertyName, '(Windows)') && ispc
                     propertyName = strrep(propertyName, '(Windows)', '');
                     propertyName = strtrim(propertyName);
-                elseif contains(propertyName, '(Unix)') && isUnix
+                elseif contains(propertyName, '(Unix)') && isUnixBased
                     propertyName = strrep(propertyName, '(Unix)', '');
                     propertyName = strtrim(propertyName);
-                elseif (strcmp(propertyName, 'JobStorageLocation.windows') && isWindows) || ... % This needs to be changed so the order of appearence in the file doesn't matter.
-                    (strcmp(propertyName, 'JobStorageLocation.unix') && isWindows)                    
+                elseif (strcmp(propertyName, 'JobStorageLocation.windows') && ispc) || (strcmp(propertyName, 'JobStorageLocation.unix') && ispc)                    
                  nextLine = fgetl(fileID);
                  eqIndex = strfind(nextLine, '=');
                                   
                  if ~isempty(eqIndex) % Check if the equals sign was found.
                     propertyValue2 = strtrim(nextLine(eqIndex+1:end));
 
-                    % Swap values if 'windows' is not found in propertyName
-                    if isempty(strfind(lower(propertyName), 'windows'))  % Ignore the readability warning because we want old MATLAB to like us.
+                    % Swap values if 'windows' is not found in propertyName.
+                    if ~contains(lower(propertyName), 'windows')
                         [propertyValue, propertyValue2] = deal(propertyValue2, propertyValue);
                     end
-                    propertyName = 'JobStorageLocation';
 
-                    % Make the struct.
-                    propertyValue = sprintf('struct(''windows'', ''%s'', ''unix'', ''%s'')', propertyValue, propertyValue2);
+                    % Make JobStorageLocation a struct to properly save this.
+                    propertyValueStruct = struct('windows', propertyValue, 'unix', propertyValue2);
+                    c.JobStorageLocation = propertyValueStruct;
+                    HasSharedFilesystem = true; % File contents take precedence over the file name.
+                    continue
                 else
                     error('The second line in JobStorageLocation struct does not contain an equals sign or is incorrectly formatted.');
                 end
@@ -141,12 +142,13 @@ try
                 if strcmp(propertyName, 'Name')
                     clusterName = propertyValue;
                     c.saveAsProfile(clusterName);
+                    continue
                 elseif contains(propertyName, 'PluginScriptsLocation')
 
-                    if versionNumberFloat >= 2.017 && versionNumberFloat < 2.0195 % Change the name in older releases.
+                    if versionNumberFloat >= 2.017 && versionNumberFloat < 2.0195 % Change the name in a certain range of older releases.
                         propertyName = strrep(propertyName, 'PluginScriptsLocation', 'IntegrationScriptsLocation');
                     elseif versionNumberFloat < 2.017
-                        continue
+                        continue % Change this to divide the files up, as they used to do this (yuck.)
                     end
 
                 elseif strcmp(propertyName, 'JobStorageLocation') && ~contains(propertyValue, 'struct')
@@ -176,17 +178,22 @@ try
                 if strcmp(propertyName, 'RequiresOnlineLicensing') && versionNumberFloat < 2.017
                     propertyName = 'RequiresMathWorksHostedLicensing';
                 end
-
-                if ~strcmp(propertyName, 'Name') % Will error if you try doing this when setting the cluster name.
-                    c.(propertyName) = propertyValue;
-                end
-
+                
+                % Temporarily store goodies in the cluster object.
+                c.(propertyName) = propertyValue;
             end
         end
     end
 
+    % Shared filesystem will be set by the filename, if it isn't set in the file's contents.
+    if contains(lower(confFileName), 'desktop.conf') && HasSharedFilesystem == false
+        c.HasSharedFilesystem = false;
+    elseif contains(lower(confFileName), 'cluster.conf') || contains(lower(confFileName), 'remote.conf') || HasSharedFilesystem == true
+        c.HasSharedFilesystem = true;
+    end
+
     % There are no AdditionalProperties in R2016b and older.
-    if versionNumberFloat >= 2.017
+    if versionNumberFloat < 2.017
         frewind(fileID); % Reset the file pointer to the beginning of the file before the second while loop. Otherwise, this whole section is ignored.
         inAdditionalPropertiesSection = false;
 
@@ -197,7 +204,7 @@ try
 
             if strcmp(line, '[AdditionalProperties]')
                 inAdditionalPropertiesSection = true;
-                continue; % Skip this line since don't need to do anything else with it.
+                continue; % Skip this line since we don't need to do anything else with it.
             end
 
             % Ignore commented or blank lines.
@@ -206,39 +213,32 @@ try
                 if numel(parts) == 2
                     propertyName = strtrim(parts{1});
                     propertyValue = strtrim(parts{2});
-
-                    % Remove any double quotation marks from propertyValue.
                     propertyValue = strrep(propertyValue, '"', '');
 
-                    % Get environment variables when used.
                     if contains(propertyValue, '$')
                         pattern = '\$(\w+)';
                         tokens = regexp(propertyValue, pattern, 'tokens');
 
                         if ~isempty(tokens)
-                            % Extract the first token which should be the environment variable name
                             envVarName = tokens{1}{1};
                             envVarValue = getenv(envVarName);
-
-                            % Replace the occurrence of the environment variable in the original string
                             propertyValue = strrep(propertyValue, ['$' envVarName], envVarValue);
                         end
                     end
 
                     % Check for OS-specific properties.
-                    if contains(propertyName, '(Windows)') && isWindows
+                    if contains(propertyName, '(Windows)') && ispc
                         propertyName = strrep(propertyName, '(Windows)', '');
                         propertyName = strtrim(propertyName);
-                    elseif contains(propertyName, '(Unix)') && isUnix
+                    elseif contains(propertyName, '(Unix)') && isUnixBased
                         propertyName = strrep(propertyName, '(Unix)', '');
                         propertyName = strtrim(propertyName);
                     elseif contains(propertyName, '(Windows)') || contains(propertyName, '(Unix)')                
-                        continue; % OS does not match, skip this property.
+                        continue;
                     end
 
                     % Set booleans and doubles correctly.
-                    if isempty(propertyValue)
-                        % Do nothing.
+                    if isempty(propertyValue) % Do nothing.        
                     elseif strcmpi(propertyValue, 'true')
                         propertyValue = true;
                     elseif strcmpi(propertyValue, 'false')
@@ -247,8 +247,7 @@ try
                         propertyValue = str2double(propertyValue);
                     end
 
-                    % Dynamically set the property on the cluster object
-                    c.AdditionalProperties.(propertyName) = propertyValue;
+                    c.AdditionalProperties.(propertyName) = propertyValue;                    
                 end
             end
         end
